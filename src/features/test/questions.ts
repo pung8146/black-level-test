7cd5b025602789c9200fbadb0f11e207aa680c94
pung8146 <pung8146@naver.com>
2025-06-13 10:47:05 +0900

    질문 목록 업데이트: 기존 질문을 새로운 질문으로 교체하여 테스트 관련 내용을 수정했습니다.

--- src/features/test/questions.ts
+++ src/features/test/questions.ts
@@ -1,7 +1,2 @@
 // src/features/test/questions.ts
-export const questions = [
-  '월급이 3일 이상 밀린 적 있나요?',
-  '수당 없이 야근한 적이 있나요?',
-  '업무 매뉴얼이 존재하지 않나요?',
-  '계약서 없이 입사하셨나요?',
-];
+export const questions = ['1번 질문', '2번 질문', '3번 질문', '4번 질문'];
