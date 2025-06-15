e7413c314f3daeaf7e01977a24f7357e2fcc40e1
pung8146 <pung8146@naver.com>
2025-06-16 07:47:37 +0900

    홈페이지 레이아웃 수정: 본문 영역에 중앙 정렬 및 여백을 추가하고, 배경색과 테두리 스타일을 개선하여 디자인을 향상시켰습니다.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko">
-      <body className="bg-gray-50 text-gray-900 min-h-screen">{children}</body>
+      <body
+        className="flex flex-col items-center justify-center 
+      bg-gray-50 text-gray-900 min-h-screen border border-gray-300 rounded-xl shadow-sm bg-white w-[500px] h-[800px] m-auto"
+      >
+        {children}
+      </body>
     </html>
   );
 }
